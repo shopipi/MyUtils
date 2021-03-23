a54e70569eea903df8e1cffa40f701018af22a26
しょ <66932484+shopipi@users.noreply.github.com>
2021-03-23 21:55:41 +0900

    Create Utils.cs

---
+++ CSharp/Utils.cs
@@ -0,0 +1,21 @@
+// グローバルIP
+public static string getGlobalIP()
+{
+    WebClient wc = new WebClient();
+    string gIP;
+
+    try
+    {
+        wc.Encoding = Encoding.UTF8;
+        string html =   wc.DownloadString("https://www.ugtop.com/spill.shtml");
+        string[] elements1 = html.Split('>');
+        string[] elements2 = elements1[42].Split('<');
+        gIP = elements2[0];
+    }
+    catch (Exception e)
+    {
+        gIP = e.Message;
+    }
+
+    return gIP;
+}
